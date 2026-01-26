c94807bbd6f4817a0914b1bf9a4dc3390c88fdf6
Tim Freudenthaler <40146720220212@litec.ac.at>
2026-01-26 09:33:44 +0100

    feat: joomla workflow

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2026-01-26 08:18:46 UTC
+#Software: Joomla! 6.0.2 Stable [ Kuimarisha ] 6-January-2026 16:00 UTC
+
+#Fields: datetime	priority	clientip	category	message
+2026-01-26T08:18:46+00:00	INFO	172.18.0.1	joomlacanceled	
+2026-01-26T08:21:58+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2026-01-26T08:22:32+00:00	INFO	172.18.0.1	joomlacanceled	
+2026-01-26T08:26:32+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
