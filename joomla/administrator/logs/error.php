3caabd064a68c6bad0d632003ca1915120b93602
Tim Freudenthaler <40146720220212@litec.ac.at>
2026-02-01 22:03:50 +0100

    feat update permissions

--- joomla/administrator/logs/error.php
+++ joomla/administrator/logs/error.php
@@ -8,3 +8,5 @@
 2026-01-26T08:21:58+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
 2026-01-26T08:22:32+00:00	INFO	172.18.0.1	joomlacanceled	
 2026-01-26T08:26:32+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2026-02-01T18:58:27+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2026-02-01T19:33:53+00:00	INFO	172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
