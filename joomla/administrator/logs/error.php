50fee153220a6bb885144b0aefdb1759d790450e
Tim Freudenthaler <40146720220212@litec.ac.at>
2026-02-08 17:57:04 +0100

    feat: joomla 1.0

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2026-02-08 15:50:01 UTC
+#Software: Joomla! 5.4.2 Stable [ Kutegemea ] 6-January-2026 16:00 GMT
+
+#Fields: datetime	priority	clientip	category	message
+2026-02-08T15:50:01+00:00	INFO	172.20.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2026-02-08T16:00:06+00:00	INFO	172.20.0.1	joomlacanceled	
+2026-02-08T16:00:34+00:00	INFO	172.20.0.1	joomlacanceled	
